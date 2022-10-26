fff0b0e9a1adeeb0853a3e7f0ae5531088acb3b4
Matheus-Nasci <matheus.nasci2002@hotmail.com>
2022-10-26 00:00:48 -0300

    lista de exercicios

--- ContaCorrente/src/main/java/com/mycompany/contacorrente/ContaCorrente.java
+++ ContaCorrente/src/main/java/com/mycompany/contacorrente/ContaCorrente.java
@@ -1,16 +1,46 @@
 package com.mycompany.contacorrente;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author matheusn
  */
 public class ContaCorrente {
+
     private String titular;
     private Double saldo;
+    private List<Historico> historico;
 
     public ContaCorrente(String titular) {
         this.titular = titular;
         this.saldo = 0.0;
+        this.historico = new ArrayList<>();
+    }
+
+    public void depositar(Double valor, Integer dia, Integer mes, Integer ano) {
+        if (valor > 0) {
+            historico.add(new Historico(dia, mes, ano, valor, "Depósito"));
+            saldo = saldo + valor;
+        }
+
+        System.out.println("VALOR INVÁLIDO");
+    }
+
+    public void sacar(Double valor, Integer dia, Integer mes, Integer ano) {
+        if (saldo > valor) {
+            historico.add(new Historico(dia, mes, ano, valor, "Saque"));
+            saldo = saldo - valor;
+        } 
+        
+        System.out.println("SALDO INSUFICIENTE");
+    }
+
+    public void exibirExtrato() {
+        for (int i = 0; i < historico.size(); i++) {
+            System.out.println(historico.get(i));
+        }
     }
 
     public String getTitular() {
@@ -31,6 +61,7 @@ public class ContaCorrente {
 
     @Override
     public String toString() {
-        return String.format(titular, args)
-    }   
+        return String.format("Titular: %s"
+                + "\nSaldo: R$ %.2f\n", titular, saldo);
+    }
 }
--- ContaCorrente/src/main/java/com/mycompany/contacorrente/Teste.java
+++ ContaCorrente/src/main/java/com/mycompany/contacorrente/Teste.java
@@ -7,6 +7,15 @@ package com.mycompany.contacorrente;
 public class Teste {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ContaCorrente c1 = new ContaCorrente("Joseph Paul");
+        
+        c1.depositar(2930.0, 25, 10, 2022);
+        c1.depositar(130.0, 25, 10, 2022);
+        c1.depositar(98.0, 25, 10, 2022);
+        c1.sacar(1560.0, 1, 02, 2023);
+        
+        c1.exibirExtrato();
+        System.out.println(c1.toString());
+        
     }
 }
